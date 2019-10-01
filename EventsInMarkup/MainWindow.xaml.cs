194c26c65bc5209b248d6b7de004b995f9ea772e
Rinat <bloomder@mail.ru>
2019-10-01 16:47:32 +0300

    add changes

---
+++ EventsInMarkup/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace EventsInMarkup
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            (sender as Button).Background = Brushes.Yellow;
+        }
+
+        private void Button_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            (sender as Button).Background = Brushes.Green;
+        }
+
+        private void Rectangle_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            (sender as Rectangle).Fill = Brushes.Yellow;
+        }
+
+        private void Rectangle_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            (sender as Rectangle).Fill = Brushes.Green;
+        }
+    }
+}
