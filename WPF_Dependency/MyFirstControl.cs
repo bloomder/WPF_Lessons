9bb4eee99cbbc2fa48ccf7afa4b8bc6518f1ec40
Rinat <bloomder@mail.ru>
2019-10-14 19:53:48 +0300

    add

---
+++ WPF_Dependency/MyFirstControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_Dependency
+{
+    class MyFirstControl : FrameworkElement
+    {
+        public static DependencyProperty DataProperty;
+        static MyFirstControl()
+        {
+            DataProperty = DependencyProperty.Register("Data", typeof(int), typeof(MyFirstControl));
+        }
+        public int Data
+        {
+            get
+            {
+                return (int)GetValue(DataProperty);
+            }
+            set
+            {
+                SetValue(DataProperty, value);
+            }
+        }
+    }
+}
