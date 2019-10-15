3fc105180304808b40732e5097ebaf483d805e42
Rinat <bloomder@mail.ru>
2019-10-15 18:23:13 +0300

    add solution

---
+++ Prcticum_DependencyProperty1/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace Prcticum_DependencyProperty1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public string TextBlock_Text
+        {
+            set
+            {
+                TextBlock1.Text = value;
+            }
+        }
+    }
+}
---
+++ Prcticum_DependencyProperty1/SimplePropClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Prcticum_DependencyProperty1
+{
+    class SimplePropClass : FrameworkElement
+    {
+        static FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata(
+            new PropertyChangedCallback(ChangedCallbackMethod),
+            new CoerceValueCallback(CoerceValueCallbackMethod));
+
+        public static DependencyProperty MyDataProperty =
+            DependencyProperty.Register(
+                "MyData",
+                typeof(int),
+                typeof(SimplePropClass),
+                metadata,
+                new ValidateValueCallback(ValidateValueCallbackMeyhod));
+
+        public int MyData
+        {
+            get
+            {
+                return (int)GetValue(MyDataProperty);
+            }
+            set
+            {
+                SetValue(MyDataProperty, value);
+            }
+        }
+
+        private static bool ValidateValueCallbackMeyhod(object value)
+        {
+            if ((int)value < 0) return false;
+            return true;
+        }
+
+        private static object CoerceValueCallbackMethod(DependencyObject d, object baseValue)
+        {
+            if ((int)baseValue <= 100) return baseValue;
+            return 100;
+        }
+
+        private static void ChangedCallbackMethod(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            
+        }
+    }
+}
